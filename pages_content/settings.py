32b6939d2665056db22dd903f07f4c66120b84a3
Serhii <furkalokbu@gmail.com>
2021-10-31 21:38:37 +0200

    add config for checkedit

--- pages_content/settings.py
+++ pages_content/settings.py
@@ -137,3 +137,167 @@ CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = "pillow"
 
+
+CKEDITOR_CONFIGS = {
+    'base': {
+        'toolbar': [
+            ['Undo', 'Redo',
+             '-', 'Bold', 'Italic', 'Underline', 'TextColor',
+             '-', 'Link',
+             '-', 'NumberedList', 'BulletedList'
+             '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
+             '-', 'Smiley',
+            ]
+        ],
+        # 'height': 500,
+        # 'width': '100%',
+        'toolbarCanCollapse': False,
+        'forcePasteAsPlainText': True
+    },
+
+    'tgmessage': {
+        'toolbar': [
+            ['Undo', 'Redo',
+             '-', 'Bold', 'Italic', 'Underline',
+             '-', 'Link',
+             '-', 'Code',
+             '-', 'EmojiPanel',
+            ]
+        ],
+        # 'height': 500,
+        # 'width': '100%', emoji
+         'extraPlugins': ','.join(
+            [
+                'emoji',
+            ]),
+        'toolbarCanCollapse': False,
+        'forcePasteAsPlainText': True
+    },
+   'tgcaption': {
+        'toolbar': [
+            ['Undo', 'Redo',
+             '-', 'Bold', 'Italic', 'Underline',
+             '-', 'Link',
+             '-', 'Code',
+             '-', 'EmojiPanel',
+            ]
+        ],
+        'height': 150,
+        # 'width': '100%', emoji
+        #  'extraPlugins': ','.join(
+        #     [
+        #         'emoji',
+        #     ]),
+        'toolbarCanCollapse': False,
+        'forcePasteAsPlainText': True
+    },
+    'default': {
+        # 'skin': 'moono',
+        'toolbar': 'FreshLime',
+        'toolbar_FreshLime': [
+            {'name': 'document', 'items': ['Source', '-', 'Save', 'NewPage', 'Preview', '-', 'Templates']},
+            {'name': 'clipboard', 'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
+            {'name': 'editing', 'items': ['Find', 'Replace', '-', 'SelectAll']},
+            {'name': 'insert',
+             'items': ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'PageBreak']},
+            '/',
+            {'name': 'basicstyles',
+             'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+            {'name': 'paragraph',
+             'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
+                       'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']},
+            {'name': 'links', 'items': ['Link', 'Unlink', 'Anchor']},
+            '/',
+            {'name': 'styles', 'items': ['Styles', 'Format', 'Font', 'FontSize']},
+            {'name': 'colors', 'items': ['TextColor', 'BGColor']},
+            {'name': 'tools', 'items': ['Maximize', 'ShowBlocks']},
+        ],
+        'autoGrow_maxHeight': '400',
+        'width': "758",
+        'height': "350",
+        'needsNbspFiller': False,
+        'extraPlugins': ','.join(
+            [
+                'div',
+                'autolink',
+                'autoembed',
+                'embedsemantic',
+                'autogrow',
+                'widget',
+                'lineutils',
+                'clipboard',
+                'dialog',
+                'dialogui',
+                'elementspath',
+                'filetools',
+                'uploadimage',
+                # 'lineheight',
+                'richcombo',
+            ]),
+        # 'filebrowserBrowseUrl': '/ckeditor/browse/',
+        # 'filebrowserUploadUrl': '/ckeditor/upload/',
+        # 'toolbarCanCollapse': True,
+        'allowedContent': True,
+        # 'line_height': '1px;2px;3px;4px;5px;6px;7px;8px;9px;10px;11px;12px;13px;14px;15px;16px;17px;18px;19px;20px;21px;22px;23px;24px;25px;26px;27px;28px;29px;30px;31px;32px;33px;34px;35px;36px;37px;38px;39px;40px;41px;42px;43px;44px;45px;46px;47px;48px;49px;50px;51px;52px;53px;54px;55px;56px;57px;58px;59px;60px;61px;62px;63px;64px;65px;66px;67px;68px;69px;70px;71px;72px',
+    },
+    'front': {
+        # 'skin': 'moono',
+        'toolbar': 'FreshLime',
+        'toolbar_FreshLime': [
+            {'name': 'clipboard', 'items': [
+                'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-',
+                'Undo', 'Redo'
+            ]},
+            {'name': 'editing', 'items': ['Find', 'Replace', '-', 'SelectAll']},
+            {'name': 'insert', 'items': [
+                'Image', 'Table', 'HorizontalRule', 'SpecialChar',
+            ]}, '/',
+            {'name': 'basicstyles', 'items': [
+                'Bold', 'Italic', 'Underline', 'Strike', '-',
+                'TextColor', 'BGColor', '-',
+                'Subscript', 'Superscript', '-',
+                'RemoveFormat', '-',
+                'NumberedList', 'BulletedList', 'Blockquote', '-',
+                'Link', 'Unlink', 'Anchor'
+            ]}, '/',
+            {'name': 'paragraph', 'items': [
+                'Outdent', 'Indent', '-',
+                'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-',
+                'BidiLtr', 'BidiRtl', '-', 'Maximize', 'ShowBlocks'
+            ]},
+            {'name': 'document', 'items': ['Source']},
+        ],
+        # 'autoGrow_maxHeight': '400',
+        'width': "99%",
+        'height': "100%",
+        'needsNbspFiller': False,
+        # 'contentsCss': [
+        #     '/static/css/fonts.min.css',
+        #     '/static/css/media.min.css',
+        #     '/static/css/main.min.css'
+        # ],
+        'extraPlugins': ','.join(
+            [
+                'div',
+                'autolink',
+                'autoembed',
+                'embedsemantic',
+                'autogrow',
+                'widget',
+                'lineutils',
+                'clipboard',
+                'dialog',
+                'dialogui',
+                'elementspath',
+                'filetools',
+                # 'uploadimage',
+                # 'lineheight',
+                'richcombo',
+            ]),
+        # 'filebrowserBrowseUrl': '/ckeditor/browse/',
+        # 'filebrowserUploadUrl': '/ckeditor/upload/',
+        # 'toolbarCanCollapse': True,
+        'allowedContent': True,
+        # 'line_height': '1px;2px;3px;4px;5px;6px;7px;8px;9px;10px;11px;12px;13px;14px;15px;16px;17px;18px;19px;20px;21px;22px;23px;24px;25px;26px;27px;28px;29px;30px;31px;32px;33px;34px;35px;36px;37px;38px;39px;40px;41px;42px;43px;44px;45px;46px;47px;48px;49px;50px;51px;52px;53px;54px;55px;56px;57px;58px;59px;60px;61px;62px;63px;64px;65px;66px;67px;68px;69px;70px;71px;72px',
+    }
+}
